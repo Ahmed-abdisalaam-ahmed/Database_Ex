ce2a620b1adedd3928032f18d0cc16b2459f0061
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2026-01-24 20:51:03 +0300

    Refactor SQL scripts and add images for Exercise 2

--- Exersice 1/Script.sql
+++ Exersice 1/Script.sql
@@ -9,33 +9,11 @@ create table users(
 	phone varchar(20)
 );
 
-select * from users
 
-insert into users (name, email, phone)
-VALUES
-('Ayaan Mohamed', 'ayaan@gmail.com',5550123),
-('Khalid Said', 'khalid@yahoo.com',5550123),
-('Nasteexo Abdi', 'nasteexo@gmail.com',5550199),
-('Sagal Osman', 'sagal@live.com',4960000),
-('Max Jones', 'max@dugsiiye.com',4971070),
-('Layla Ahmed', 'layla123@gmail.com',6862200),
-('Mohamed Ibrahim', 'moha@backend.com',6969770),
-('Zahra Hassan', 'zahra@outlook.com',9960000);
 
--- Exersice 1 
+SELECT * FROM users
+WHERE (email LIKE '%gmail.com' OR email LIKE '%hotmail.com') AND id > 2;
 
--- # 1
-select * from users where name like '%d'
--- # 2
-select email from users;
--- #3
-select * from users 
-where email like  '%o%'
---  #4 
-select * from users
-where name like '____%'
---  #5
-select phone from users
 
 
 
